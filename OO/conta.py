e5f589c32616769287470c52d79914581a1495a3
Henrique Alves <henryworkdev.ti@gmail.com>
2023-01-22 21:40:22 -0300

    Alterando arquivos

--- OO/conta.py
+++ OO/conta.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Criando a conta do titular: {}".format(titular))
@@ -6,28 +7,52 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
-    
+
     def extrato(self):
         print("\n------Extrato da Conta-------")
         print("Número:{}".format(self.__numero))
         print("Titular:{}".format(self.__titular))
-        print("Saldo:{}".format(self.__saldo))
+        print(str("Saldo:R$:%.2f" % self.__saldo).replace('.', ','))
+        print(str("Limite disponível:R$:%.2f"%self.__limite).replace('.', ','))
+        print(str("Limite Total:R$:%.2f"%(self.__limite+self.__saldo)).replace('.', ','))
         data = datetime.now()
-        print("Data:{}".format(data.strftime('%d/%m/%Y %H:%M')))
-    
+        print("Data/Hora:{}".format(data.strftime('%d/%m/%Y - %H:%M')))
+
+    def __limite_disponivel(self, valor):
+        limite_disponivel = self.__saldo + self.__limite
+        return valor <= limite_disponivel
+
+    def __atualiza_saldo_limite(self, valor):
+        if (self.__saldo >= valor):
+            self.__saldo -= valor
+        else:
+            if(self.__saldo != 0):
+                self.__limite -= valor - self.__saldo
+                self.__saldo = 0
+            else:
+                self.__limite -= valor 
+
     def depositar(self, valor):
         self.__saldo += valor
 
     def sacar(self, valor):
-        self.__saldo -= valor
+        if(self.__limite_disponivel(valor)):
+            self.__atualiza_saldo_limite(valor)
+        else:
+            print("\nFalha ao sacar:Limite indisponível!")
+        
     
     def alterarLimite(self, valor):
         self.limite = valor
     
     def transferir(self, conta, valor):
-        conta.depositar(valor)
-        self.__saldo -= valor
-
+        if(self.__limite_disponivel(valor)):
+            conta.depositar(valor)
+            self.__atualiza_saldo_limite(valor) 
+        else:
+            print("\nFalha ao transferir:Limite indisponível!")
+       
+    
 # getters e setters
     def get_titular(self):
         return self.__titular
--- OO/contas.py
+++ OO/contas.py
@@ -8,8 +8,6 @@ conta1.depositar(100.0)
 conta2.depositar(200.0)
 
 conta1.extrato()
-conta2.extrato()
 
-# mosntrando o limite da conta formatado
-limite = "O limite da conta {} é: R$ %.2f".format(conta1.get_numero())%conta1.get_limite()
-print(limite.replace('.', ','))
+conta1.sacar(700.0)
+conta1.extrato()
