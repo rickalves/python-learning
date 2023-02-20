6a5673ecd146a852d349424962d5fa7d6d1a1996
Henrique Alves <henryworkdev.ti@gmail.com>
2023-02-19 22:39:36 -0300

    manipulando arquivos

--- src/basics/files.py
+++ src/basics/files.py
@@ -1,19 +1,24 @@
 #Leitura e Escrita de arquivos em python
 
-arquivo = open('./basics/citys.txt', 'w')#criando o arquivo em modo de escrita (Write)
+arquivo = open('./src/basics/citys.txt', 'w')#criando o arquivo em modo de escrita (Write)
 arquivo.write('Gotham\n')
 arquivo.write('New York\n')
 arquivo.write('Sao Paulo\n')
 arquivo.close()
-arquivo = open('citys.txt', 'a')#abrindo o arquivo em modo de Append para escrever novamente no arquivo
+arquivo = open('./src/basics/citys.txt', 'a')#abrindo o arquivo em modo de Append para escrever novamente no arquivo
 arquivo.write('Rio de Janeiro\n')
+arquivo.write('Guaraciaba do Norte\n')
 arquivo.close()
 
 # Abrindo arquivos em modo de leitura
-arquivo_nomes = open('./basics/names.txt', 'r')#abrindo o arquivo em modo de leitura (Read)
+arquivo_nomes = open('./src/basics/names.txt', 'r')#abrindo o arquivo em modo de leitura (Read)
 nomes = []
 for nome in arquivo_nomes:
     nome = nome.strip()
     nomes.append(nome)
+
 arquivo_nomes.close()
-print(nomes[len(nomes)-1])
+# escrevendo os nomes
+for nome in nomes:
+    print(nome)
+
